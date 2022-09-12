789eaf5fa27fcba310743e5b062aaa70624716b8
astritdemiri11 <astritdemiri11@gmail.com>
2022-09-13 01:03:10 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { INTERNAL_FEATURE, LanguageCode, LanguageService, TranslateService } from 'ngx-material-translate';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngx-material-translate';
+export class AppComponent implements OnInit {
+  subscription: Subscription | null;
+
+  constructor(
+    public languageService: LanguageService,
+    public translateService: TranslateService) {
+    this.subscription = null;
+  }
+
+  ngOnInit() {
+    this.languageService.business.selectLanguage(LanguageCode.English);
+    this.languageService.business.loadLanguages();
+
+    this.translateService.business.loadTranslations(INTERNAL_FEATURE);
+    this.translateService.business.loadTranslations('app');
+    this.translateService.business.loadTranslations('feature');
+
+    this.subscription = this.languageService.model.activeLanguageCode$.subscribe(languageCode => {
+      const lastActiveLanguageCode = this.languageService.business.getLastActiveLanguageCode();
+
+      if (lastActiveLanguageCode !== languageCode) {
+        this.translateService.business.loadTranslations(INTERNAL_FEATURE);
+        this.translateService.business.loadTranslations('app');
+        this.translateService.business.loadTranslations('feature');
+        this.languageService.business.translateLanguages();
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { MaterialTranslateModule, TranslationLoaderService } from 'ngx-material-translate';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +13,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    EffectsModule.forRoot(),
+    HttpClientModule,
+    MaterialTranslateModule.forRoot({
+      loader: { provide: TranslationLoaderService, useFactory: (httpClient: HttpClient) => new TranslationLoaderService(httpClient), deps: [HttpClient] },
+      translationLoadingClass: 'blurOut',
+      translationSuccessClass: 'blurIn'
+    }),
+    StoreModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent]
